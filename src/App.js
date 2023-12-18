d2c1ce2b4ce3e880421205d04d4f5dcc95f818d7
demmix <dominikmalicki1@gmail.com>
2023-12-18 20:12:13 +0100

    adding add frame

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
 
 import './App.css';
+import React from 'react';
 import dominik from './media/dominiktcg.jpg';
-import jakub from './media/jakub.jpg';
 import mucha from './media/mucha.jpg';
 import ogramto from './media/ogramto.jpg';
 import dorian from './media/dorian.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        To jest strona Rodzinki TCG <br></br>
-        Niestety jest jeszcze w budowie <br></br>
-        Zapraszamy na nasze kanały na yt: <br></br>
-        <div>
-          <a href="https://www.youtube.com/@Jakub-Kolodziej" 
-            target="_blank"
-            rel="noreferrer">
-              <img src={jakub} alt="jakub" />
-          </a>
-          <a href="https://www.youtube.com/@DominikTCG"
-            target="_blank"
-            rel="noreferrer">
-              <img src={dominik} alt="dominik" />
-          </a>
-          <a href="https://www.youtube.com/@Ogramto"             
-            target="_blank"
-            rel="noreferrer">
-              <img src={ogramto} alt="ogramto" />
-          </a>
-          <a href="https://www.youtube.com/@shadow_pkmn"
-            target="_blank"
-            rel="noreferrer">
-              <img src={dorian} alt="dorian" />
-          </a>
-          <a href="https://www.youtube.com/@pokemucha"            
-            target="_blank"
-            rel="noreferrer">
-              <img src={mucha} alt="mucha" />
-          </a>
-        </div>
-      </header>
-    </div>
-  );
-}
+
+ function App() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          To jest strona Rodzinki TCG <br></br>
+          Niestety jest jeszcze w budowie <br></br>
+          Zapraszamy na nasze kanały na yt: <br></br>
+          <div>
+          <iframe height="90px" width="310px" frameborder="0" src="https://socialcounts.org/youtube-live-subscriber-count/UCJaGHkADXSuGyFD6L4Skdjg/embed" allowFullScreen></iframe>
+            <a href="https://www.youtube.com/@DominikTCG"
+              target="_blank"
+              rel="noreferrer">
+                <img src={dominik} alt="dominik" />
+            </a>
+            <p> <span className="sub-count"></span></p>
+            <a href="https://www.youtube.com/@Ogramto"             
+              target="_blank"
+              rel="noreferrer">
+                <img src={ogramto} alt="ogramto" />
+            </a>
+            <a href="https://www.youtube.com/@shadow_pkmn"
+              target="_blank"
+              rel="noreferrer">
+                <img src={dorian} alt="dorian" />
+            </a>
+            <a href="https://www.youtube.com/@pokemucha"            
+              target="_blank"
+              rel="noreferrer">
+                <img src={mucha} alt="mucha" />
+            </a>
+          </div>
+        </header>
+      </div>
+    );
+  }
 
 export default App;
